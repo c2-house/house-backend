85dd44864e06be9052a2d9c8d09c3771c4a30ef3
Jungminchae <minchae3618@gmail.com>
2022-06-03 23:10:33 +0900

    add parameterize to myhome test

--- apis/tests/test_house_apis.py
+++ apis/tests/test_house_apis.py
@@ -5,9 +5,11 @@ from apis.tests.utils import create_myhome_data
 pytestmark = pytest.mark.django_db
 
 
-def test_read_myhome_with_cs(client):
-    target = "신혼부부"
-    region = "서울특별시"
+@pytest.mark.parametrize(
+    "target, region",
+    [("신혼부부", "서울특별시"), ("신혼부부", "경기도"), ("대학생", "서울특별시"), ("대학생", "경기도")],
+)
+def test_read_myhome_with_cs(client, target, region):
     url = reverse("myhome-list") + f"?target={target}&region={region}"
 
     create_myhome_data(target, region, 10)
@@ -15,5 +17,5 @@ def test_read_myhome_with_cs(client):
     response = client.get(url)
     response_data = response.json()
 
-    assert response_data["results"][0]["region"] == "서울특별시"
+    assert response_data["results"][0]["region"] == region
     assert response.status_code == 200
