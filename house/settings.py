e1eb1017fb43b1a6705b5d359df791725c32762d
Jungminchae <minchae3618@gmail.com>
2022-06-08 13:57:03 +0900

    add django q & redis setup

--- house/settings.py
+++ house/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-davw5=_o3stee4chayth7@(+!$*#f(tt_l2noai8*0l#7_3tf#"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,7 +41,7 @@ INSTALLED_APPS = [
 ]
 PROJECT_APPS = ["apis"]
 
-THIRD_PARTY_APPS = ["rest_framework"]
+THIRD_PARTY_APPS = ["rest_framework", "django_q"]
 
 INSTALLED_APPS += PROJECT_APPS + THIRD_PARTY_APPS
 
@@ -80,8 +81,12 @@ WSGI_APPLICATION = "house.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": config("DB_HOST", "localhost"),
+        "NAME": config("DB_NAME", "admin"),
+        "USER": config("DB_USER", "root"),
+        "PASSWORD": config("DB_PASSWORD", "root"),
+        "PORT": config("DB_PORT", "5432"),
     }
 }
 
