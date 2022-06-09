afa1a6afb4ccb3c14c643682e7a1bb04b7c507e8
Jungminchae <minchae3618@gmail.com>
2022-06-09 13:38:02 +0900

    change max_length in MyHome model

--- apis/models.py
+++ apis/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    supply_type = models.CharField(max_length=10, verbose_name="공급유형")
+    supply_type = models.CharField(max_length=50, verbose_name="공급유형")
     title = models.CharField(max_length=255, verbose_name="공고명")
     registration_date = models.DateField(verbose_name="모집공고 일자")
     supplier = models.CharField(max_length=50, verbose_name="공급 기관")
@@ -18,9 +18,9 @@ class SH(models.Model):
 
 class MyHome(BaseModel):
     status = models.BooleanField(verbose_name="진행상태")
-    region = models.CharField(max_length=10, verbose_name="지역")
+    region = models.CharField(max_length=50, verbose_name="지역")
     release_date = models.DateField(verbose_name="당첨발표 일자")
-    target = models.CharField(max_length=10, verbose_name="대상자")
+    target = models.CharField(max_length=50, verbose_name="대상자")
 
 
 class StationArea(BaseModel):
